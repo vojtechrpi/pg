a3c055e1fb1743dbb34fc40090e7bdade1272ad7
vojtechrpi <vojtech.ads@gmail.com>
2024-10-31 12:26:25 +0000

    postup1

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
 import sys
+import csv
 
 def nacti_csv(soubor):
-    pass
+    seznam = []
+    with open(soubor, "r") as file:
+        reader = csv.reader(file)
+        for radek in reader:
+            seznam.append(print(radek))
+    return seznam
 
+# def spoj_data(*data):
+#     return [["Tomas","Novak","28","1"], ["Jan","Dvorak","20","1"], ["Alice","Nova","","1"]]
 
-def spoj_data(*data):
-    pass
 
 
 def zapis_csv(soubor, data):
-    pass
+    with open(soubor, "w") as fp:
+        writer = csv.writer(fp)
+        writer.writerows(data)
+
+    
 
 
 if __name__ == "__main__":
@@ -20,5 +30,9 @@ if __name__ == "__main__":
         csv_data2 = nacti_csv(soubor2)
         vysledna_data = spoj_data(csv_data1, csv_data2)
         zapis_csv(vysledna_data)
-    except Exception:
-        pass
+    except IndexError:
+        print("chyba")
+        
+data = [["Tomas","Novak","28","1"], ["Jan","Dvorak","20","1"], ["Alice","Nova","","1"]]
+nacti_csv("excel.csv")
+zapis_csv("novy_excel.csv", data)
