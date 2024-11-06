24e947a8df3ff7f934966f5fa48fb55c0f822bb7
vojtechrpi <vojtech.ads@gmail.com>
2024-11-06 15:24:35 +0000

    fifth_ukol

--- fifth.py
+++ fifth.py
@@ -1,7 +1,5 @@
-
 import sys
 
-# definice úvodních binárních sekvencí obrázkových souborů
 jpeg_header = b'\xff\xd8'
 gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
@@ -9,48 +7,29 @@ png_header = b'\x89PNG\r\n\x1a\n'
 
 
 def read_header(file_name, header_length):
-    """
-    Tato funkce načte binární soubor z cesty file_name,
-    z něj přečte prvních header_length bytů a ty vrátí pomocí return
-    """
-    return b'xxx'
+    try:
+        with open(file_name, "rb") as otevreny_soubor:
+            return otevreny_soubor.read(header_length)
+    except FileNotFoundError:
+        raise Exception(f"Soubor '{file_name}' nebyl nalezen.")
 
 
 def is_jpeg(file_name):
-    """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
-    tu srovná s definovanou hlavičkou v proměnné jpeg_header
-    """
-    # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
-
-    # vyhodnoť zda je soubor jpeg
-
-    return False
+    return header.startswith(jpeg_header)
 
 
 def is_gif(file_name):
-    """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
-    tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
-    """
-    # vyhodnoť zda je soubor gif
-    return False
+    header = read_header(file_name, len(gif_header1))
+    return header.startswith(gif_header1) or header.startswith(gif_header2)
 
 
 def is_png(file_name):
-    """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
-    tu srovná s definovanou hlavičkou v proměnné png_header
-    """
-    # vyhodnoť zda je soubor png
-    return False
+    header = read_header(file_name, len(png_header))
+    return header.startswith(png_header)
 
 
 def print_file_type(file_name):
-    """
-    Funkce vypíše typ souboru - tuto funkci není třeba upravovat
-    """
     if is_jpeg(file_name):
         print(f'Soubor {file_name} je typu jpeg')
     elif is_gif(file_name):
@@ -62,6 +41,10 @@ def print_file_type(file_name):
 
 
 if __name__ == '__main__':
-    # přidej try-except blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except IndexError:
+        print("Chyba: Zadej název souboru jako argument.")
+    except Exception as e:
+        print(f"Chyba: {e}")
